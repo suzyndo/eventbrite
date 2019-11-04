a6040c3d34ba208ecd5d440c38098f155287af2a
Suzy <suzy.nzekwu@gmail.com>
2019-11-04 19:45:52 +0100

    ultime essai

--- eventbrite/config/environment.rb
+++ eventbrite/config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :password => ENV['SENDGRID_PWD'],
   :domain => 'monsite.fr',
   :address => 'smtp.sendgrid.net',
-  :port => 587,
+  :port => 465,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
